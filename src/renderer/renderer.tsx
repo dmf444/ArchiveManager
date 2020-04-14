1060a2b09b7b7c7558b5c1eed3541d6bc3e1ef30
dmf444 <becat444@gmail.com>
2020-04-14 18:09:55 -0400

    Some minor work on learning the rendering system in React!

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,16 +1,151 @@
 /**
  * React renderer.
  */
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 // Import the styles here to process them with webpack
+import {
+    FileAddOutlined,
+    HomeOutlined,
+    SettingOutlined
+} from '@ant-design/icons';
 import '@public/style.css';
+import { Layout, Menu } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <div className='app'>
-    <h4>Welcome to React, Electron and Typescript</h4>
-    <p>Hello</p>
-  </div>,
-  document.getElementById('app')
+    <Layout>
+        <Sider collapsible
+            style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0
+            }}
+        >
+            <div className='logo' />
+            <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+                <Menu.Item key='1'>
+                    <HomeOutlined />
+                    <span className='nav-text'>Home</span>
+                </Menu.Item>
+                <Menu.Item key='2'>
+                    <FileAddOutlined />
+                    <span className='nav-text'>Files</span>
+                </Menu.Item>
+                <Menu.Item key='3'>
+                    <SettingOutlined />
+                    <span className='nav-text'>Settings</span>
+                </Menu.Item>
+            </Menu>
+        </Sider>
+        <Layout className='site-layout' style={{ marginLeft: 200 }}>
+            <Header className='site-layout-background' style={{ padding: 0 }} />
+            <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                <div className='site-layout-background' style={{ padding: 24, textAlign: 'center' }}>
+                    ...
+                    <br />
+                    Really
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    long
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    ...
+                    <br />
+                    content
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+    </Layout>,
+    document.getElementById('app')
 );

3d00f496a32acaf50435d81e3dd3b71216a277ee
pat-aoc <patrick.ocampo@mail.utoronto.ca>
2020-04-21 15:28:59 -0400

    added titles for the statuses

--- src/renderer/components/home/Status.tsx
+++ src/renderer/components/home/Status.tsx
@@ -2,19 +2,58 @@
 import * as React from 'react';
 
 import '@public/style.css';
-import { Layout } from 'antd';
+import { Layout, Row, Col, Typography } from 'antd';
 
 const { Header, Content, Footer, Sider } = Layout;
+const { Title } = Typography;
+
 import 'antd/dist/antd.css';
+import '../../style/home/Status.css';
 
 export class Status extends React.Component {
     
+    constructor(props) {
+        super(props)
+
+        // perform a fetch for the status
+    }
+
     render() {
         return(
             <div className="statusComp">
+                <Row gutter={[40, 16]} justify="start">
+                    <Col flex={4} span={8}>
+                        <div className="discStatus">
+                            <Row justify="start">
+                                <Col>
+                                    <Title level={4}>Discord Status</Title>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
 
+                    <Col flex={4} span={8}>
+                        <div className="dbStatus">
+                            <Row justify="start">
+                                <Col>
+                                    <Title level={4}>Database Status</Title>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
 
+                    <Col flex={4} span={8}>
+                        <div className="tbd">
+                            <Row justify="start">
+                                <Col>
+                                    <Title level={4}>Placeholder Status</Title>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
             </div>
         );
     }
 }
+
